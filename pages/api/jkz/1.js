bf43df685d387a1c0f24502c406e788b9716b285
chenjinmei51 <chenjinmei51@gmail.com>
2025-11-11 13:06:05 +0800

    Update 1.js

--- pages/api/jkz/1.js
+++ pages/api/jkz/1.js
@@ -3,7 +3,7 @@ export const config = { runtime: 'edge' };
 
 export default async function handler() {
   const target =
-    'https://www.integrativenutrition.com/health-coaching-101?utm_channel=Referral&utm_medium=SocialLadder&utm_source=ReferAFriend&campaign_idnum__c=701TQ000004HrdXYAS&ambassadorID=ea1f50bb-2942-451a-8f35-25a4f14fbecd&sldiscountcode=YONGLINWUXIIN';
+    'https://www.integrativenutrition.com/course-catalog?utm_channel=Ambassador&utm_medium=IINAmbassador&ambassadorID=160e6193-b033-4de0-aa95-97e32adde221&sldiscountcode=JACKZANZANXIIN';
 
   return new Response(null, {
     status: 302,
